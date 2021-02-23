18bf0683011a37b132484a6f60b01d053c98f5fa
ahmedseragcodes <ahmedserag1@gmail.com>
2021-02-23 16:35:43 -0500

    wrote login event handler, utilizing history and local storage, have errors displaying on screen

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import React, { useState } from "react";
-import { Link, Route } from "react-router-dom";
+import { Link, Route, useHistory } from "react-router-dom";
 import styled from "styled-components";
+import axios from 'axios';
+import axiosWithAuth from "./utilz/axiosWithAuth";
 
 
 function App() {
+
+  const history=useHistory();
 
   const [formValues, setFormValues]=useState({
     username: "",
     password: "", 
   })
 
-  const [errors, setErrors]=useState("");
+  const [errors, setErrors]=useState([]);
 
   const [friends, setFriends]=useState([]);
 
@@ -26,12 +30,31 @@ function App() {
 
   }
 
+  const login = (event) => {
+    event.preventDefault();
+    axios.post("http://localhost:5000/api/login", formValues)
+    .then((res)=>{
+      console.log("APP COMP, LOGIN EVENT HANDLER, AXIOS  SUCCESS", res);
+      localStorage.setItem("token", JSON.stringify(res));
+      history.push("/login");
+    })
+    .catch((err)=>{
+      console.log("APP COMP, LOGIN EVENT HANDLER, AXIOS FAIL", err);
+      setErrors([
+        err.response.data.error
+      ])
+    })
+    
+
+
+  }
+
   return (
     <FrontPage >
       <HeaderNav>
         <ul>
           <li>
-            <Link to="/">Login</Link>
+            <Link to="/login">Login</Link>
           </li>
           <li>
             <Link>Logout</Link>
@@ -42,15 +65,18 @@ function App() {
         </ul>
       </HeaderNav>
       <LoginForm>
-        <form>
+        <form onSubmit={login}>
           <label htmlFor="username">Username:
-            <input name="username" id="username" placeholder="Enter Your Username" />
+            <input name="username" id="username" placeholder="Enter Your Username" onChange={handleChange} value={formValues.username} />
           </label>
           <label htmlFor="password">Password:
-            <input name="password" id="password" placeholder="Enter Your Password" />
+            <input name="password" id="password" placeholder="Enter Your Password" onChange={handleChange} value={formValues.password} />
           </label>
+        <button>Log In</button>
+        {errors.map((error)=>{
+          return <p>{error}</p>
+        })}
         </form>
-        <button>Login</button>
       </LoginForm>
     </FrontPage>
   );
